e7ac6cbda7918a7cf4b07ba0c96b01152b7d2a61
Waleed Rehman <waleedrehmankhan@outlook.com>
2020-11-16 15:35:59 +1100

    Event Controller Complete

---
+++ PlatterFusion.API/PlatterFusion.API/Controllers/EventController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PlatterFusion.API.Dtos;
+using PlatterFusion.API.Helpers;
+using PlatterFusion.API.Model;
+using PlatterFusion.API.Persistence;
+using PlatterFusion.API.Persistence.Repositories.Event;
+
+namespace PlatterFusion.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+
+        #region Configuration
+
+        private readonly ILogger<EventController> _logger;
+        private IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public EventController(ILogger<EventController> logger, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        #endregion
+
+        [HttpPost("All")]
+        public async Task<ContentResult> GetEvent(GetEventInput input)
+        {
+            try
+            {
+                ReturnMessage rm = new ReturnMessage(1, "Success");
+                var events = await Task.Run(() => _unitOfWork.Events.GetAsync(filter: e => input.Id != 0 ? (e.Id == input.Id) : true));
+                var eventsToReturn = _mapper.Map<IEnumerable<EventDto>>(events);
+
+                return this.Content(rm.returnMessage(new PagedResultDto<EventDto>
+                    (eventsToReturn.AsQueryable(), input.pagenumber, input.pagesize)),
+                    "application/json");
+
+            }
+            catch (Exception ex)
+            {
+                return this.Content(JsonConvert.SerializeObject(new
+                {
+                    // 0 is Exception
+                    msgCode = 0,
+                    msg = ex.Message
+                }), "application/json");
+            }
+        }
+
+        [HttpPost("GetById")]
+        public async Task<ContentResult> GetEventById(GetEventInput input)
+        {
+            return await GetEvent(input);
+        }
+
+        [HttpPost("CreateOrUpdate")]
+        [ValidateFilter]
+        public async Task<ContentResult> CreateOrUpdateEvent(EventDto eventDto)
+        {
+            ReturnMessage rm = new ReturnMessage(1, "Success");
+
+            try
+            {
+                var events = await Task.Run(() => _unitOfWork.Events.GetAsync(filter: e => e.Id == eventDto.Id));
+                var eventToAdd = _mapper.Map<Event>(eventDto);
+
+                if (events.Count() == 0)
+                {
+                    eventDto.Id = 0;
+                    _unitOfWork.Events.Add(eventToAdd);
+                }
+                else
+                {
+                    _unitOfWork.Events.Update(eventToAdd);
+                }
+
+                var status = _unitOfWork.Complete();
+                _logger.LogInformation("Log:Add Event for ID: {Id}", eventToAdd.Id);
+                return this.Content(rm.returnMessage(null),
+                         "application/json");
+            }
+            catch (Exception ex)
+            {
+                rm.msg = ex.Message.ToString();
+                rm.msgCode = 0;
+                return this.Content(rm.returnMessage(null));
+            }
+        }
+
+        [HttpPost("Delete")]
+        public async Task<ContentResult> DeleteEvent(GetEventInput input)
+        {
+            ReturnMessage rm = new ReturnMessage(1, "Success");
+
+            try
+            {
+                var events = await Task.Run(() => _unitOfWork.Events.GetAsync(filter: e => e.Id == input.Id));
+
+                if (events.Count() == 0)
+                {
+                    rm.msgCode = -1;
+                    rm.msg = "Not Found";
+                }
+                else
+                {
+                    _unitOfWork.Events.Remove(events.First());
+                    _unitOfWork.Complete();
+                }
+
+                _logger.LogInformation("Log:Delete Event for ID: {Id}", input.Id);
+                return this.Content(rm.returnMessage(null),
+                            "application/json");
+            }
+            catch (Exception ex)
+            {
+                rm.msg = ex.Message.ToString();
+                rm.msgCode = 0;
+                return this.Content(rm.returnMessage(null));
+            }
+        }
+    }
+}
---
+++ PlatterFusion.API/PlatterFusion.API/ValidateFilterAttribute.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatterFusion.API
+{
+    public class ValidateFilterAttribute : ResultFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            base.OnResultExecuting(context);
+            dynamic message = new JObject();
+            if (!context.ModelState.IsValid)
+            {
+                var totallist = context.ModelState.ToList();
+                _ = context.ModelState.Values.ToList();
+
+                foreach (var item in totallist)
+                {
+                    var errors = item.Value.Errors.ToList();
+                    var errorarray = new JArray();
+                    for (int i = 0; i < errors.Count; i++)
+                    {
+                        JValue error = new JValue(errors[i].ErrorMessage);
+                        errorarray.Add(error);
+                    }
+                    message.Add(item.Key, errorarray);
+                }
+                dynamic returnMessage = new JObject();
+                returnMessage.msgCode = -2;
+                returnMessage.msg = message;
+
+
+                context.Result = new OkObjectResult(new
+                {
+                    data = new { items = new { }, message = returnMessage },
+                });
+            }
+        }
+    }
+}
